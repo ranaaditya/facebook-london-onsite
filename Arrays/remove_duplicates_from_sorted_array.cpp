5e3b44ea8cd39fd46c121b574ece630781c79be6
ranaaditya <mail2aditya.rana98@gmail.com>
2021-12-25 14:05:43 +0530

    remove duplicates from sorted array

---
+++ Arrays/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,75 @@
+/**
+ * @file remove_duplicates_from_sorted_array.cpp
+ * @author Aditya Rana
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+/**
+
+* Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that
+* each unique element appears only once. The relative order of the elements should be kept the same.
+
+* Since it is impossible to change the length of the array in some languages, you must instead have the
+* result be placed in the first part of the array nums. More formally, if there are k elements after removing
+* the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+* Return "k" after placing the final result in the first k slots of nums.
+
+* Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+* Test Cases:
+
+* #1
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+
+* #2
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+ */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// input array is already sorted in non-descending(ascending) order.
+int removeDuplicates(vector<int>& input) {
+
+    if(input.size() == 0) return 0;
+    if(input.size() == 1) return 1;
+
+    int i = 0, j = 0, k = 0;
+    int length = input.size();
+
+    while(i < length) {
+        j = i;
+        for(j = i+1; j < length; j++) {
+            if(input[j] == input[i]) { continue; }
+            else break;
+        }
+        input[k] = input[i];
+        k++;
+        i = j; 
+    }
+
+    //debug purpose
+    // for(auto x : input) {
+    //     cout << x;
+    // }
+
+    return k;
+}
+
+int main() {
+    //vector<int> input = {1,1,2};
+    vector<int> input = {0,0,1,1,1,2,2,3,3,4};
+    cout << removeDuplicates(input);
+    return 0;
+}
