1c0954f65cc4e91a9f436ee14fdd80d77412529e
ranaaditya <mail2aditya.rana98@gmail.com>
2022-01-06 16:04:17 +0530

    update Hoares partitioning method in kth smallest element in an array

--- Arrays/kth_smallest_element_in_array.cpp
+++ Arrays/kth_smallest_element_in_array.cpp
@@ -28,11 +28,30 @@ int LomutoPartition(vector<int>& ip, int low, int high) {
     return i;
 }
 
-int HoaresPartition(vector<int> v, int left, int right) {
+int HoaresPartition(vector<int> ip, int low, int high) {
+    int pivot = ip[low];
+    int i = low-1;
+    int j = high+1;
+    
+    while(true) {
+        do{
+            i++;
+        }while(ip[i] < pivot);
+
+        do{
+            j--;
+        }while(ip[j] > pivot);
+
+        if(i >= j){
+            return j;
+        }
+
+        swap(ip[i], ip[j]);
+    }
     return -1;
 }
 
-int KthSmallest(vector<int> input, int left, int right, int k) {
+int KthSmallestLomutoPartitioning(vector<int> input, int left, int right, int k) {
     if(left == right)
     return input[left];
 
@@ -49,16 +68,34 @@ int KthSmallest(vector<int> input, int left, int right, int k) {
         left = pivotIndex+1;
     }
 
-    return KthSmallest(input, left, right, k);
+    return KthSmallestLomutoPartitioning(input, left, right, k);
 }
+
+int KthSmallestHoaresPartitioning(vector<int> input, int left, int right, int k) {
+    if(left == right)
+    return input[left];
+
+    int pivotIndex = HoaresPartition(input, left, right);
+
+    if(k == pivotIndex)
+    return input[pivotIndex];
+
+    else if(pivotIndex > k){
+        right = pivotIndex-1;
+    }
+
+    else {
+        left = pivotIndex+1;
+    }
+
+    return KthSmallestHoaresPartitioning(input, left, right, k);
+}
+
+
 int main() {
     vector<int> ip = {7,-2,5,8,1,6};
-    int i = LomutoPartition(ip, 0, 5);
-    for(auto x : ip)
-    cout << x << "\t";
 
-    cout << endl <<  "pivot index : " << i << endl;
-
-    cout << KthSmallest(ip, 0,5,2);
+    cout << KthSmallestLomutoPartitioning(ip, 0,5,2) << endl;
+    cout << KthSmallestHoaresPartitioning(ip,0,5,2) << endl;
     return 0;
 }
