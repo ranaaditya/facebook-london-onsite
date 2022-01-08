bdeab47693096b0f0b55008fe2f104d7be963bc3
ranaaditya <mail2aditya.rana98@gmail.com>
2022-01-09 04:53:10 +0530

    multiple with 0 and 1

---
+++ Math/multiple_with_0_and_1.cpp
@@ -0,0 +1,66 @@
+/**
+ * @file multiple_with_0_and_1.cpp
+ * @author Aditya Rana
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief You are given an integer N. You have to find smallest multiple of N which consists of digits 0 and 1 only. 
+ * Since this multiple could be large, return it in form of a string.
+ * 
+ * TestCase:
+ * 
+ * #1:
+ * Input: n = 3
+ * 
+ * Output: 111
+ * 
+ * #2:
+ * 
+ * Input: n = 18
+ * 
+ * @param n 
+ * @return string 
+ */
+string smallestMultipleWithZeroAndOne(int n) {
+    string output = "1";
+    string outputL = "1", outputR = "1";
+    string left, right;
+    long long unsigned int leftLL, rightLL;
+    while(true) {
+        left.clear(); right.clear();
+        left = outputL + "0";
+        right = outputR + "1";
+        leftLL = atoi(left.c_str());
+        rightLL = atoi(right.c_str());
+
+        cout << "DEBUG: \t" << "LEFT: " << leftLL << endl;
+        cout << "DEBUG: \t" << "RIGHT: " << rightLL << endl;
+
+        if(rightLL % n == 0) {
+            output = right;
+            break;
+        }
+        else if(leftLL % n == 0) {
+            output = left;
+            break;
+        }
+        outputL = left;
+        outputR = right;
+    }
+    return output;
+}
+
+int main() {
+    int n = 12;
+    cout << smallestMultipleWithZeroAndOne(n) <<  endl;
+    return 0;
+}
