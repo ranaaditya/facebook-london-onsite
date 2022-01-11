637fdb00c6d2e4287797dc3ab3298890481ae988
ranaaditya <mail2aditya.rana98@gmail.com>
2022-01-11 17:08:23 +0530

    add: problem statement

--- BinarySearch/random_pick_with_weights.cpp
+++ BinarySearch/random_pick_with_weights.cpp
@@ -9,6 +9,40 @@
  * 
  */
 
+/**
+ * Problem:
+ * 
+ * You are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
+ * 
+ * You need to implement the function pickIndex(), which randomly picks an index in the range [0, w.length - 1] (inclusive) and returns it. The probability of picking an index i is w[i] / sum(w).
+ * 
+ * For example, if w = [1, 3], the probability of picking index 0 is 1 / (1 + 3) = 0.25 (i.e., 25%), and the probability of picking index 1 is 3 / (1 + 3) = 0.75 (i.e., 75%).
+ * 
+ * TestCases:
+ * 
+ * #1:
+ * 
+ * Input: ["Solution","pickIndex"]
+ * 
+ * [[[1]],[]]
+ * 
+ * Output: [null,0]
+ * 
+ * Explanation:
+ * 
+ * Solution solution = new Solution([1]);
+ * 
+ * solution.pickIndex(); // return 0. The only option is to return 0 since there is only one element in w.
+ * 
+ * #2:
+ * 
+ * Input: ["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+ * 
+ * [[[1,3]],[],[],[],[],[]]
+ * 
+ * Output: [null,1,1,1,1,0]
+ * 
+ */
 #include<bits/stdc++.h>
 using namespace std;
 
