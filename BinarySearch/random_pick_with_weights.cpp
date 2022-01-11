cffd22bdf84a72ea773542f91562ca10f7cafb4a
ranaaditya <mail2aditya.rana98@gmail.com>
2022-01-11 18:02:18 +0530

    add more accuraterandomizer function

--- BinarySearch/random_pick_with_weights.cpp
+++ BinarySearch/random_pick_with_weights.cpp
@@ -46,6 +46,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+float gerenrateRandom() {
+    random_device rd;
+    default_random_engine generator(rd());
+    uniform_real_distribution<> range(0,1);
+    return (float)range(generator);
+}
+
 int pickRandom(vector<int> nums) {
     int sum = 0;
     vector<int> prefixSum;
@@ -55,15 +62,15 @@ int pickRandom(vector<int> nums) {
 
     for(auto x : prefixSum) cout << x << "\t";
     float randNum = (float) rand() / RAND_MAX;
-    cout << rand << endl;
-    float target = randNum * prefixSum.back();
-    cout << target << endl;
+    //cout << rand << endl;
+    float target = gerenrateRandom() * prefixSum.back();
+    //cout << target << endl;
 
     //Need to find upper bound of target in prefixSum array
-    for(int i = 0 ; i < prefixSum.size(); i++){
-        if(target < prefixSum[i])
-        return i;
-    }
+    // for(int i = 0 ; i < prefixSum.size(); i++){
+    //     if(target < prefixSum[i])
+    //     return i;
+    // }
 
     //one liner using STL
     // return upper_bound(prefixSum.begin(), prefixSum.end(), target) - prefixSum.begin();
@@ -73,6 +80,7 @@ int pickRandom(vector<int> nums) {
 
 int main() {
     vector<int> nums = {1,2,3,4,3};
+    for(int i = 0 ; i < 5; i++)
     cout << pickRandom(nums) << endl;
     return 0;
 }
