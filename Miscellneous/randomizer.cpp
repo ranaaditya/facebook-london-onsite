8970dcda623c1dadfff0779513dc86043002ca2b
ranaaditya <mail2aditya.rana98@gmail.com>
2022-01-13 05:17:01 +0530

    update randomizer reproducibility and docs

--- Miscellneous/randomizer.cpp
+++ Miscellneous/randomizer.cpp
@@ -9,6 +9,32 @@
  * 
  */
 
+/**
+ * std::random_device device
+ * 
+ * This is a special class which produces uniformly-distributed unsigned integers with 32 bits of length. 
+ * It can produce random numbers either by accessing the operational system's entropy pool via system calls 
+ * or by using hardware random number generators such as Intel's RdRand when available 
+ * (but not all implementations of std::random_device allow this). Developers must be warned, however, that even 
+ * though 32-bit unsigned integers are generated, the entropy of this random number generator may be lower than 32 bits.
+ * 
+ * Unfortunately, it is not advisable to use std::random_device repeatedly as this may quickly deplete the entropy 
+ * pool and therefore reduce the level of randomness available to the other processes running in the system. Additionally, 
+ * its reliance on system calls makes it a very slow random number generator.
+ * 
+ * use std::random_device to generate a single random number which is then used as a seed for a pseudorandom number 
+ * generator (PRNG) and then use the PRNG itself to generate as many pseudorandom numbers as we wish.
+ * 
+ * 
+ * std::uniform_int_distribution
+ * 
+ * Produces random integer values i, uniformly distributed on the closed interval [a,b], 
+ * that is, distributed according to the discrete probability function.
+ * 
+ * Using its overloaded operator(), an object of type std::uniform_int_distribution can take a random number generator and use 
+ * it to generate a number within its defined target interval. Indeed, the following expression in our code returns a number in  [a,b]
+ * 
+ */
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -46,6 +72,14 @@ char randomizer(char low, char high) {
 
     return distribution(generator) + 'a';
 
+}
+
+int reproducibleRandomizer(int low, int high) {
+/* seed the PRNG (MT19937) using a fixed value (in our case, 0) */
+    mt19937 generator(0);
+    uniform_int_distribution<int> distribution(low, high);
+
+    return distribution(generator);
 }
 
 int main() {
